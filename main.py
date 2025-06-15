9aecdb2b06ebb508b9e8d6d9062d62845a6d8b09
NeoDimi <dimitri.lanari@gmail.com>
2025-06-15 16:28:27 +0200

    5. Added the report printing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, list_dict_characters, sort_list
 
 def get_book_text(file_path: str) -> str:
     #Reads the content of a book file and returns it as a string.
@@ -10,8 +10,19 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     words_count = count_words(book_text)
-    print(f"{words_count} words found in the document.")
     character_dict = count_characters(book_text)
-    print(f"{character_dict}")
+    #print(f"{character_dict}")
+    list_dict_unsorted = list_dict_characters(character_dict)
+    list_dict_sorted = sort_list(list_dict_unsorted)
+
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {book_path}...")
+    print(f"----------- Word Count ----------\nFound {words_count} total words")
+    print("--------- Character Count -------")
+    for d in list_dict_sorted:
+        if d["name"].isalpha():
+            print(f"{d["name"]}: {d["count"]}")
+    #print(list_dict_sorted)
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,22 @@ def count_characters(text:str) -> dict:
         else:
             chars[lowered] = 1
     return chars
+
+def list_dict_characters(chars:dict) -> list:
+    list_char_dicts = []
+    for char in chars:
+        char_dict ={
+            "name": char,
+            "count": chars[char]
+        }
+        list_char_dicts.append(char_dict)
+    return list_char_dicts
+
+def sort_on(dict:dict):
+    return dict["count"]
+
+def sort_list(list_dict:list) -> list:
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
+#.sort() is an "in-place method" meaning it replaces what is has been given as input and returns a type "None".
+#This is why we cannot store the result of .sort() in a new variable. It automatically erased its input with the output.
